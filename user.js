c0a510394445e95daba21b3e2fee5f0212ba9160
Sbeas0317 <sebastiansandoval12371@gmail.com>
2023-07-13 20:31:12 -0500

    Co-authored-by: JPWhiteOfficial <JPWhiteOfficial@users.noreply.github.com>

--- user.js
+++ user.js
@@ -1,55 +1,19 @@
-/** @format */
-
 const users = JSON.parse(localStorage.getItem("users"));
-const container = document.getElementById("user-container");
+const container = document.getElementById("card-container");
 
-users.forEach((user) => {
+users.forEach((user, index) => {
   container.innerHTML += `
-      <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 18rem;">
       <div class="card-body">
         <h5 class="card-title">${user.name}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${user.surname}</h6>
         <h6 class="card-text">${user.email}</h6>
         <h6 class="card-text">${user.phone}</h6>
-      <a href="#" class="card-link">Eliminar</a>
-      <a href="#" class="card-link">Editar</a>
+        <a href="#" class="card-link">Eliminar</a>
+        <button type="button" id="openModalBtn${index}" class="btn btn-primary" data-toggle="modal" data-target="#myModal" data-email="${user.email}">
+          Editar
+        </button>
+      </div>
     </div>
-  </div>
-    `;
-  // container.appendChild(userItem);
+  `;
 });
-const userData = JSON.parse(localStorage.getItem("users")) ?? [];
-
-container.addEventListener("click", (e) => {
-  //console.log(e.composedPath()[1].childNodes[5].childNodes[0].data);
-
-  const deleteText = e.target.innerText;
-
-  const id = e.composedPath()[1].childNodes[5].childNodes[0].data;
-
-  if (e.target.innerText === deleteText) {
-    const findUser = userData.findIndex((find) => find.email === id);
-
-    if (findUser !== -1) {
-      userData.splice(findUser, 1);
-
-      localStorage.setItem("users", JSON.stringify(findUser));
-      e.target.parentNode.parentNode.remove();
-    }
-  }
-});
-const deleteUser = () => {};
-const deleteBtn =(id,deleteText)=>{
-  if(e.target.innerText === deleteText){
-    const userData = JSON.parse( localStorage.getItem("users"))??[]
-    const findUser= userData.findIndex((find)=>{
-      find.email===id
-    })
-   if(findUser != -1){
-    userData.slice(findUser, 1)
-    deleteUser(userData); 
-
-   }
-
-  }
-}
